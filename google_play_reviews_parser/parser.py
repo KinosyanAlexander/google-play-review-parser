3ed39e1527b6a6c7581a309744a748c7e3b192d6
KinosyanSasha <kinosyanz@gmail.com>
2020-08-07 23:47:48 +0300

    Avoid recursion, some architectural changes, docs added

--- google_play_reviews_parser/parser.py
+++ google_play_reviews_parser/parser.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from requests import post
+import requests
 import time
 import re
 import sqlite3 as sq
@@ -14,6 +14,10 @@ from google_play_reviews_parser.utils import make_request_data, Review, get_toke
 
 
 class Reviews():
+    '''
+    Class, wich allow parse reviews
+    and write reviews in sqlite base
+    '''
     def __init__(self,
                 app_id,
                 lang='ru',
@@ -24,10 +28,7 @@ class Reviews():
                 sqlbase=None):
         self.app_id = app_id
         self.lang = lang
-        self.all_count = count
-        self.count = self.all_count % 199
-        if not self.count:
-            self.count = 199
+        self.change_count(count)
         self.score = score
         self.sort = sort
         self.token = start_token
@@ -42,16 +43,62 @@ class Reviews():
 
         if self.sqlbase:
             self.create_base()
-        
+    
+    def change_count(self, count):
+        '''
+        Change count of reviews
+        '''
+        self.all_count = count
+        self.count = count % 199
+        if not self.count:
+            self.count = 199
+    
+    def refresh(self):
+        '''
+        Restart info about Review
+        '''
+        self.data = []
+        self.token = None
+        self.play_parsing = False
+        self.change_count(self.all_count)
+        self.format_post_data(is_start=not self.token)
+        self.url = Url.API_URL.format(lang=self.lang)
+        if self.sqlbase:
+            self.create_base()
+    
+    def change_lang(self, lang):
+        '''
+        Change reviews language
+        '''
+        self.lang = lang
+        self.url = Url.API_URL.format(lang=self.lang)
     
     def request(self):
-        request = post(self.url,
-                    data=self.post_data,
-                    headers={"content-type": "application/x-www-form-urlencoded"}
-                )
-        return request
+        '''
+        Make post request to take reviews
+        '''
+        try:
+            request = requests.post(self.url,
+                        data=self.post_data,
+                        headers={"content-type": "application/x-www-form-urlencoded"},
+                        timeout=0.5
+                    )
+            return request
+        except requests.exceptions.RequestException:
+            try:
+                return self.request()
+            except RecursionError:
+                return None
 
     def format_post_data(self, is_start=False):
+        '''
+        Formatting post data to make request
+        '''
+        if self.score is None:
+            self.score = 'null'
+        if self.sort is None:
+            self.sort = 'null'
+
         if is_start:
             self.post_data = PostData.POST_DATA_FOR_FIRST_REQUEST.format(
                 sort=self.sort,
@@ -70,10 +117,16 @@ class Reviews():
         return self.post_data
     
     def extract_request_data(self, request):
+        '''
+        Extracting reviews data from post request from self.request()
+        '''
         data = make_request_data(request.text)
         return data
     
     def create_base(self, filename=''):
+        '''
+        Create Sqlite db for reviews
+        '''
         if not self.sqlbase:
             self.sqlbase = filename if filename else f'{self.app_id}_base'
         
@@ -92,8 +145,12 @@ class Reviews():
         self.conn.commit()
     
     def add_reviews_to_base(self, data):
+        '''
+        Adding reviews from data (=> List of Review) to sqlite base
+        if base exists
+        '''
         try:
-            data = map(lambda x: x.get_list(), data)
+            data = map(lambda x: x.get_list(for_base=True), data)
             data = list(filter(lambda x: x, data))
             self.cursor.executemany('''
                    INSERT INTO reviews VALUES
@@ -106,6 +163,9 @@ class Reviews():
             return False
 
     def make_review(self, element):
+        '''
+        Make Review() object from review info data
+        '''
         nick = element[1][0]
         text = element[4]
         score = element[2]
@@ -118,8 +178,16 @@ class Reviews():
 
         return review
 
-    def get_reviews(self):
+    def take_some_reviews(self):
+        '''
+        Func, that allow ONE request parsing
+        '''
         self.play_parsing = True
+
+        if not self.token:
+            self.format_post_data(is_start=True)
+        else:
+            self.format_post_data(is_start=False)
 
         request = self.request()
 
@@ -129,12 +197,10 @@ class Reviews():
         except (SyntaxError, ValueError, TypeError):
             token = get_token_from_text(request.text)
             if token:
-                print(self.token)
                 self.token = token
                 print('\n\nnew_token\n\n', self.token)
-                self.count = 199
                 self.format_post_data()
-                return self.get_reviews()
+                return self.token
             else:
                 print('Sorry, something get wrong with extracting data\n')
                 print(self.token)
@@ -142,18 +208,24 @@ class Reviews():
                 # if input('Do you want to watch text? [y/n]') == 'y':
                 #     print(clear_request_data(request.text))
                 print('That is over, token cant be extracted')
+                self.play_parsing = False
                 return self.data
-
+        
         reviews = list(map(self.make_review, reviews_data))
         reviews = list(filter(lambda x: x, reviews))
-        self.data += reviews
-
+        
         if self.sqlbase:
-            self.add_reviews_to_base(reviews)
+            try:
+                self.add_reviews_to_base(reviews)
+                self.data += reviews
+            except UnicodeEncodeError:
+                print('Problem with data coding')
+                pass
 
         if len(self.data) >= self.all_count:
             self.play_parsing = False
             print(f'All {len(self.data)} reviews parsed')
+            return self.data
 
         try:
             self.token = data[1][1]
@@ -165,11 +237,21 @@ class Reviews():
         
         if self.play_parsing:
             print(f'{len(self.data)}   reviews parsed')
-            self.count = 199
-            self.format_post_data()
-            return self.get_reviews()
-        else:
-            return self.data
+            if (self.all_count - self.count) % 199:
+                self.count = 199
+    
+    def get_reviews(self):
+        '''
+        Main func, that allow MANY requests parsing
+        and fill self.data for reviews for self.all_count count
+        '''
+        self.play_parsing = True
+        while self.play_parsing:
+            self.take_some_reviews()
+        
+        self.token = None
+        return self.data
+    
 
 
 
